628c722ad1c593e1913cb2f728c434404dca27c1
josh <copiga@copiga.co.uk>
2015-02-20 22:46:49 +0000

    initial commit

---
+++ base/Debug/main.pp
@@ -0,0 +1,59 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/time.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_hibernate.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/fpu.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/hibernate.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/systick.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/utils/uartstdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/utils/cmdline.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/examples/boards/ek-tm4c123gxl/drivers/rgb.h
+main.obj: C:/ti/TivaWare_C_Series-1.1/examples/boards/ek-tm4c123gxl/drivers/buttons.h
+main.obj: ../main.h
+
+../main.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/time.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-1.1/inc/hw_hibernate.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/hibernate.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-1.1/utils/uartstdio.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-1.1/utils/cmdline.h: 
+C:/ti/TivaWare_C_Series-1.1/examples/boards/ek-tm4c123gxl/drivers/rgb.h: 
+C:/ti/TivaWare_C_Series-1.1/examples/boards/ek-tm4c123gxl/drivers/buttons.h: 
+../main.h: 
---
+++ base/Debug/utils/cmdline.pp
@@ -0,0 +1,21 @@
+# FIXED
+
+utils/cmdline.obj: C:/ti/TivaWare_C_Series-1.1/utils/cmdline.c
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+utils/cmdline.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/string.h
+utils/cmdline.obj: C:/ti/TivaWare_C_Series-1.1/utils/cmdline.h
+
+C:/ti/TivaWare_C_Series-1.1/utils/cmdline.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/string.h: 
+C:/ti/TivaWare_C_Series-1.1/utils/cmdline.h: 
