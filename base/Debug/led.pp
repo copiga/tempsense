53f8b27963c8bfa99b176ad5b0caf83313c6919c
josh <copiga@copiga.co.uk>
2015-02-21 19:36:25 +0000

    added error blink for LED

--- base/Debug/led.pp
+++ base/Debug/led.pp
@@ -8,6 +8,7 @@ led.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
 led.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
 led.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
 led.obj: C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+led.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
 led.obj: C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
 led.obj: ../defines.h
 led.obj: ../led.h
@@ -20,6 +21,7 @@ C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h:
 C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
 C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
 C:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
 C:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
 ../defines.h: 
 ../led.h: 
--- base/led.c
+++ base/led.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 
 #include "defines.h"
@@ -9,4 +10,15 @@
 void ledSetColour(uint8_t colour)
 {
 	GPIOPinWrite(GPIO_PORTF_BASE, RED_LED|BLUE_LED|GREEN_LED, colour);
+}
+
+void errorBlink(uint8_t colour, int rate)
+{
+    while(1)
+    {
+        ledSetColour(colour);
+        SysCtlDelay(rate);
+        ledSetColour(0);
+        SysCtlDelay(rate);
+    }
 }
