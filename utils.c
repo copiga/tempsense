9e31965bde0704bf6839c3549e822879b54131d5
copiga <ben11107138@coleg-powys.ac.uk>
2015-03-02 03:21:08 +0000

    stylistic changes

--- utils.c
+++ utils.c
@@ -10,28 +10,28 @@ void floatString(float input, char *out)
     int Cnum;
     
     if(input < 0)
-	out[pos++]='-';
+	out[pos++] = '-';
     while(input >= 10.00)
     {
-	input/=10.0;
+	input /= 10.0;
 	places++;
     }
-    for(x=0;x<3;x++)
+    for(x = 0;x < 3;x++)
     {
-	Cnum = (int)input;
-	input-=Cnum;
+	Cnum = (int) input;
+	input -= Cnum;
 	if(Cnum > 9)
 	    goto err;
 	//ascii code for the current number
-	out[pos++] = 48+Cnum;
-	if(places==0)
+	out[pos++] = 48 + Cnum;
+	if(places == 0)
 	    out[pos++] = '.';
-	input*=10;
+	input *= 10;
 	places--;
     }
     
   fin:
-    out[x+1]=0;
+    out[x + 1] = 0;
     return;
     
   err:
