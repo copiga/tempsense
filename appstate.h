c9e8da14030f133e23ebd47b56b9c5f49b7685fa
copiga <ben11107138@coleg-powys.ac.uk>
2015-03-06 03:12:33 +0000

    padded structs

--- appstate.h
+++ appstate.h
@@ -4,11 +4,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+
 struct appstate
 {
-    bool ledOn;
-    uint8_t ledColour;
-    uint8_t probeCount;
+    int ledOn:1;
+    int master:1;
+    unsigned int ledColour:8;
+    unsigned int probeCount:8;
+    unsigned int probeID:8;
     float temp;
 };
 #endif
+//padinfo
+//32 bit word
+//word aligned
