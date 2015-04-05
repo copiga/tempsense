ce12277b7246e2a47ed5023678683c7f5d6a5efd
copiga <ben11107138@coleg-powys.ac.uk>
2015-04-05 23:30:37 +0100

    effector output is now PB2

--- effector.c
+++ effector.c
@@ -3,16 +3,24 @@
 #include "inc/hw_memmap.h"
 #include "driverlib/rom.h"
 #include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
 #include "led.h"
+
+void enableEffector(void)
+{
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_2);
+}
+    
 
 void outputOn(int output)
 {
-    ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, GPIO_PIN_2);
+    ROM_GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void outputOff(int output)
 {
-    ROM_GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, 0);
+    ROM_GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_2, 0);
     return;
 }
